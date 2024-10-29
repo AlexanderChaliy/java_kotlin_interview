638ee0f9d6dde98740bd9247285033a42a279942
Oleksandr Chalyi <olexandr.chalyi@atola.com>
2024-10-29 18:21:24 +0200

    added tasks for kotlin

--- src/main/java/CustomStack.java
+++ src/main/java/CustomStack.java
@@ -1,13 +1,21 @@
-public class CustomStack  {
-    public void push(int item){
+public class CustomStack {
+    public void push(int x) {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
-    public int pop(){
+    public int pop() {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
     public int getMax(){
         throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void insert(int position, int item){
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public int size(){
+        return 3;
     }
 }
